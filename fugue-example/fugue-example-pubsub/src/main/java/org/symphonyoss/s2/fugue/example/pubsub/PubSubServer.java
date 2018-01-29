4d8ea0715621f4856c70ab96b9e89dc0fc77c9ad
bruceskingle <bruce.skingle@symphony.com>
2018-01-28 18:45:28 -0800

    PubSub example can create topic

--- fugue-example/fugue-example-pubsub/src/main/java/org/symphonyoss/s2/fugue/example/pubsub/PubSubServer.java
+++ fugue-example/fugue-example-pubsub/src/main/java/org/symphonyoss/s2/fugue/example/pubsub/PubSubServer.java
@@ -23,6 +23,8 @@
 
 package org.symphonyoss.s2.fugue.example.pubsub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.symphonyoss.s2.common.http.IServletContainer;
 import org.symphonyoss.s2.common.http.IServletProvider;
 import org.symphonyoss.s2.fugue.core.FugueServer;
@@ -30,8 +32,16 @@ import org.symphonyoss.s2.fugue.di.IComponent;
 import org.symphonyoss.s2.fugue.di.IDIContext;
 import org.symphonyoss.s2.fugue.di.impl.ComponentDescriptor;
 
+import com.google.api.gax.rpc.ApiException;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.pubsub.v1.TopicAdminClient;
+import com.google.pubsub.v1.TopicName;
+
 public class PubSubServer extends FugueServer implements IComponent, IServletProvider
 {
+  private static final String TEST_TOPIC = "Test-Topic";
+  
+  private static final Logger log_ = LoggerFactory.getLogger(PubSubServer.class);
 
   public PubSubServer(IDIContext diContext, String name)
   {
@@ -43,13 +53,44 @@ public class PubSubServer extends FugueServer implements IComponent, IServletPro
   {
     return super.getComponentDescriptor()
         .addProvidedInterface(IServletProvider.class)
-        .addStart(() -> openBrowser())
+//        .addStart(() -> openBrowser())
         .addStart(() -> startPubSub());
   }
 
   private void startPubSub()
   {
+    createTopic();
+  }
+
+  private void createTopic()
+  {
+    log_.info("About to create topic");
     
+    // Your Google Cloud Platform project ID
+    String projectId = ServiceOptions.getDefaultProjectId();
+
+    // Your topic ID, eg. "my-topic"
+    String topicId = TEST_TOPIC;
+
+    // Create a new topic
+    TopicName topic = TopicName.of(projectId, topicId);
+    try (TopicAdminClient topicAdminClient = TopicAdminClient.create())
+    {
+      topicAdminClient.createTopic(topic);
+    }
+    catch (ApiException e)
+    {
+      // example : code = ALREADY_EXISTS(409) implies topic already exists
+      log_.error("Failed to create topic, HTTP {} retryable {}", e.getStatusCode().getCode(), 
+          e.isRetryable(), e);
+    }
+    catch (Exception e)
+    {
+      log_.error("Failed to create topic", e);
+    }
+    
+
+    log_.info("Topic {}:{} created.", topic.getProject(), topic.getTopic());
   }
 
   @Override
