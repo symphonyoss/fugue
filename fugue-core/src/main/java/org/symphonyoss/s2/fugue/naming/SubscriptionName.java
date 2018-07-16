05a2f9d032206061a3e5de71f1b64364efa68f70
bruceskingle <bruce.skingle@symphony.com>
2018-07-16 10:21:01 +0100

    Add external subscription name

--- fugue-core/src/main/java/org/symphonyoss/s2/fugue/naming/SubscriptionName.java
+++ fugue-core/src/main/java/org/symphonyoss/s2/fugue/naming/SubscriptionName.java
@@ -26,10 +26,13 @@ package org.symphonyoss.s2.fugue.naming;
 /**
  * The name of a subscription.
  * 
- * This is constructed from the subscription simple name and the topic name, in that order
+ * This is normally constructed from the topic name and the subscription simple name, in that order
  * so that AWS policies can be written with resource rules of the form ${ENVIRONMENT}-*
  * 
- * SBE creates subscriptions with the topic followed by the simple subscription name.
+ * In situations where the subscription may belong to an external environment, it is necessary to construct
+ * the name from the subscription simple name and the topic name, in that order. This is done by calling the
+ * newExternalInstance static factory method.
+ * 
  * @author Bruce Skingle
  *
  */
@@ -39,14 +42,37 @@ public class SubscriptionName extends Name
   private final String subscription_;
 
   /**
-   * Constructor.
+   * Factory method for internal (normal) subscription names.
    * 
    * @param topicName     The TopicName of the topic to be subscribed to.
    * @param subscription  The simple subscription name.
+   * 
+   * @return A new SubscriptionName 
    */
-  public SubscriptionName(TopicName topicName, String subscription)
+  public static SubscriptionName newInstance(TopicName topicName, String subscription)
   {
-    super(subscription, topicName.toString());
+    return new SubscriptionName(topicName, subscription, topicName.toString(), subscription);
+  }
+  
+  /**
+   * Factory method for external subscription names.
+   * 
+   * In situations where the subscription may belong to an external environment, it is necessary to construct
+   * the name from the subscription simple name and the topic name, in that order.
+   * 
+   * @param topicName     The TopicName of the topic to be subscribed to.
+   * @param subscription  The simple subscription name.
+   * 
+   * @return A new SubscriptionName 
+   */
+  public static SubscriptionName newExternalInstance(TopicName topicName, String subscription)
+  {
+    return new SubscriptionName(topicName, subscription, subscription, topicName.toString());
+  }
+  
+  private SubscriptionName(TopicName topicName, String subscription, String part1, String part2)
+  {
+    super(part1, part2);
     
     topicName_ = topicName;
     subscription_ = subscription;
