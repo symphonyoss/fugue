ec410065af7a3bcdfe6fd7fd666af47fc5dbfbfb
bruceskingle <bruce.skingle@symphony.com>
2019-06-20 15:55:28 +0100

    Javadoc fix

--- fugue-core/src/main/java/org/symphonyoss/s2/fugue/store/ObjectExistsException.java
+++ fugue-core/src/main/java/org/symphonyoss/s2/fugue/store/ObjectExistsException.java
@@ -41,7 +41,7 @@ public class ObjectExistsException extends Exception
    * @param  message the detail message (which is saved for later retrieval
    *         by the {@link #getMessage()} method).
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A <i>null</i> value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    */
