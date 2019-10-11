d4dff57cdce276487adc768aee53f30e49fb2c1a
bruceskingle <bruce.skingle@symphony.com>
2019-10-11 15:32:52 +0100

    Prepare for release

--- fugue-core/src/main/java/org/symphonyoss/s2/fugue/pipeline/FatalConsumerException.java
+++ fugue-core/src/main/java/org/symphonyoss/s2/fugue/pipeline/FatalConsumerException.java
@@ -56,14 +56,14 @@ public class FatalConsumerException extends Exception
 
   /**
    * Constructs a new exception with the specified cause and a detail
-   * message of <tt>(cause==null ? null : cause.toString())</tt> (which
-   * typically contains the class and detail message of <tt>cause</tt>).
+   * message (which
+   * typically contains the class and detail message of cause).
    * This constructor is useful for exceptions that are little more than
    * wrappers for other throwables (for example, {@link
    * java.security.PrivilegedActionException}).
    *
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A null value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    */
@@ -81,7 +81,7 @@ public class FatalConsumerException extends Exception
    * @param  message the detail message (which is saved for later retrieval
    *         by the {@link #getMessage()} method).
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A null value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    */
--- fugue-core/src/main/java/org/symphonyoss/s2/fugue/pipeline/RetryableConsumerException.java
+++ fugue-core/src/main/java/org/symphonyoss/s2/fugue/pipeline/RetryableConsumerException.java
@@ -70,14 +70,14 @@ public class RetryableConsumerException extends Exception
 
   /**
    * Constructs a new exception with the specified cause and a detail
-   * message of <tt>(cause==null ? null : cause.toString())</tt> (which
-   * typically contains the class and detail message of <tt>cause</tt>).
+   * message of (cause==null ? null : cause.toString()) (which
+   * typically contains the class and detail message of cause).
    * This constructor is useful for exceptions that are little more than
    * wrappers for other throwables (for example, {@link
    * java.security.PrivilegedActionException}).
    *
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A null value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    */
@@ -97,7 +97,7 @@ public class RetryableConsumerException extends Exception
    * @param  message the detail message (which is saved for later retrieval
    *         by the {@link #getMessage()} method).
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A null value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    */
@@ -162,14 +162,14 @@ public class RetryableConsumerException extends Exception
 
   /**
    * Constructs a new exception with the specified cause and a detail
-   * message of <tt>(cause==null ? null : cause.toString())</tt> (which
-   * typically contains the class and detail message of <tt>cause</tt>).
+   * message of (cause==null ? null : cause.toString()) (which
+   * typically contains the class and detail message of cause).
    * This constructor is useful for exceptions that are little more than
    * wrappers for other throwables (for example, {@link
    * java.security.PrivilegedActionException}).
    *
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A null value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    * @param  retryTimeUnit The units of the given retryTime
@@ -192,7 +192,7 @@ public class RetryableConsumerException extends Exception
    * @param  message the detail message (which is saved for later retrieval
    *         by the {@link #getMessage()} method).
    * @param  cause the cause (which is saved for later retrieval by the
-   *         {@link #getCause()} method).  (A <tt>null</tt> value is
+   *         {@link #getCause()} method).  (A null value is
    *         permitted, and indicates that the cause is nonexistent or
    *         unknown.)
    * @param  retryTimeUnit The units of the given retryTime
